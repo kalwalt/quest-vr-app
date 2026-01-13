7bfba56a3879fe07762e937bf45177dc90213950
kalwalt <github@kalwaltart.it>
2026-01-14 00:39:32 +0100

    feat: update package.json and vite.config.ts for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  base: './', 
+  base: '/quest-vr-app/', 
   plugins: [
     basicSsl()
   ],
