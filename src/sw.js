f009a461f49539269493b822490a8105140e2441
kalwalt <github@kalwaltart.it>
2026-01-14 01:06:36 +0100

    Updates

--- src/sw.js
+++
@@ -1,9 +0,0 @@
-// A minimal Service Worker that allows installation
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-self.addEventListener('fetch', (e) => {
-// Pass all requests directly to the network (no caching for now)
-// Useful in development to see 3D changes immediately
-  e.respondWith(fetch(e.request));
-});
